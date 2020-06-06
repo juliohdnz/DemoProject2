c609c37e6b256d4e11851eef8fe2d381a07f9d49
juliohdnz <juliohdnz@gmail.com>
2020-06-05 21:05:39 -0500

    Primer archivo

---
+++ main.js
@@ -0,0 +1,23 @@
+//inicializar Git en la terminal y crear un archivo de practica
+// comando ls = desplega todo el contenido de la carpeta donde te encuentras
+//localizarse en la carpeta de pracica
+
+//creamos un nueva aplicacion
+const name = 'Hector'
+
+//guardamos
+// inicializar repositorio >> git master
+//Aparecen letras a un lado del nombre del archivo y dice untracked 
+//Cada que se haga algun cambio , Git detecta cambios en el directorio de trabajo
+//Hay que reportar a git que cambios se han hecho
+//Dos pasos 1) Agregar el archivo al staging area
+    //2) Agregarlo al repo
+
+//Checar el estatus del repositorio > git status
+//Cuando lo muestra en rojo quiere decr que ningun archivo esta  agregado y que esta en untracked
+//checar el status del user > git config user.name
+//checar el status del mail > git config user.email
+// git config --global user.name "Hector Hdez" >> Cambiar nombre
+//git config --global user.email "juliohdnz@gmail.com" >> Cambiar mail
+
+
